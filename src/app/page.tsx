e53f13bbd7edd77a3c9e9bde7870c322fd30f18f
Jacob Georgis <jacob.georgis@student.manchester.ac.uk>
2024-03-07 15:53:23 +0000

    Add SVGR plugin and SVG logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import About from "@/components/sections/about";
 import FAQ from "@/components/sections/faq";
 import Schedule from "@/components/sections/schedule";
+import Alt1 from "./alt 1.svg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* <h1 className="text-6xl font-bold">MCRStudentHack</h1> */}
+
+      <Alt1 className="transition ease-in-out delay-150 hover:scale-110" />
       <About />
       <Schedule />
       <FAQ />
