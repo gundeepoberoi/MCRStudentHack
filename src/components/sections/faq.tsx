60b9207ee172387ae80a47bf312bfe226edf3c1b
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-03-19 15:14:39 +0000

    chore: update file

--- src/components/sections/faq.tsx
+++ src/components/sections/faq.tsx
@@ -62,7 +62,7 @@ export default function FAQ() {
           w-full max-h-[40rem] overflow-auto scrollbar-hide backdrop-blur-sm
           pt-8 transition-all`}
         >
-          <h3 className="text-2xl text-center font-bold px-8 pb-4">
+          <h3 className="text-5xl text-center font-bold px-8 pb-4">
             Frequently Asked Questions
           </h3>
           {faqQuestions.map((faq, i) => {
@@ -95,8 +95,8 @@ export default function FAQ() {
           }
           bg-transparent backdrop-blur p-8 transition-all`}
         >
-          <h3 className="text-2xl font-bold pb-4">Answer</h3>
-          <p className="whitespace-pre-wrap">
+          <h3 className="text-4xl font-bold pb-4">Answer</h3>
+          <p className="whitespace-pre-wrap text-lg">
             {selectedId >= 0 ? faqQuestions[selectedId].answer : ""}
           </p>
         </div>
