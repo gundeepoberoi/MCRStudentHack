7cb0f2a11a072afc4dd6c8860caf729f31b5a700
Dhruv <59387969+druvdub@users.noreply.github.com>
2024-03-19 20:17:14 +0000

    Minor refactoring

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -13,41 +13,41 @@ const Hero = () => {
       id="hero"
       className="flex min-h-screen w-full flex-col items-center justify-center p-4 relative mb-12"
     >
-      <>
-        <div className="flex flex-col lg:flex-row justify-center items-center w-full gap-1 lg:gap-36">
-          <div>
-            <Image
-              src={"/alt_1.png"}
-              width={600}
-              height={600}
-              alt={"Logo"}
-              className="w-full max-w-[500px] transition ease-in-out delay-150 hover:scale-110 content-center"
-            />
-          </div>
-          <div>
-            <form action="https://apply.mcrstudenthack.com/">
-              <input type="hidden" name="r" id="r" value="apply" />
-              <Button type="submit">Register Now</Button>
-            </form>
-          </div>
+      <div className="flex flex-col lg:flex-row justify-center items-center w-full gap-1 lg:gap-36">
+        <div>
+          <Image
+            src={"/alt_1.png"}
+            width={600}
+            height={600}
+            alt={"Logo"}
+            className="w-full max-w-[500px] transition ease-in-out delay-150 hover:scale-110 content-center"
+          />
         </div>
+        <div>
+          <form action="https://apply.mcrstudenthack.com/">
+            <input type="hidden" name="r" id="r" value="apply" />
+            <Button type="submit" containerClassName="bg-cyan-950">
+              Register Now
+            </Button>
+          </form>
+        </div>
+      </div>
 
-        <div className="flex flex-col" suppressHydrationWarning={true}>
-          <div className="mt-16">
-            <TypewriterEffectSmooth
-              className="justify-center"
-              words={[
-                {
-                  text: "Registration",
-                  className: "text-cyan-100",
-                },
-                { text: "Closes in:", className: "text-cyan-300" },
-              ]}
-            />
-          </div>
-          <TimerContainer currentDate={new Date()} />
+      <div className="flex flex-col" suppressHydrationWarning={true}>
+        <div className="mt-16">
+          <TypewriterEffectSmooth
+            className="justify-center"
+            words={[
+              {
+                text: "Registration",
+                className: "text-cyan-100",
+              },
+              { text: "Closes in:", className: "text-cyan-300" },
+            ]}
+          />
         </div>
-      </>
+        <TimerContainer currentDate={new Date()} />
+      </div>
     </section>
   );
 };
