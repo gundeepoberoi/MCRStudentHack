170d02b3d4a8a565881c16a060cecffd9d079944
h69005am <aadityaa.mehrotra@student.manchester.ac.uk>
2024-03-20 10:25:22 +0000

    fixed alignment and responsiveness, added booking logo

--- src/components/sections/sponsors/sponsors.tsx
+++ src/components/sections/sponsors/sponsors.tsx
@@ -4,14 +4,15 @@ import './sponsors.css';
 
 export default function Sponsors() {
     return (
-      <section id="sponsors" className="mt-48 mb-24 w-full z-10">
-        <div className="flex flex-wrap w-full text-white rounded-lg overflow-hidden">
+      <section id="sponsors" className="mt-48 mb-24 w-full z-10 flex flex-col items-center">
+        <h1 id="heading" className="text-5xl text-center">Sponsors</h1>
+        <div id="divider" className="flex flex-wrap w-full text-white rounded-lg overflow-hidden">
           <div className="sun"></div>
           <div className="planet" style={{animationDelay: '0s'}}></div>
-          <div className="planet" style={{animationDelay: '-4s'}}></div>
-          <div className="planet-1" style={{animationDelay: '-8s'}}></div>
-          <div className="planet-1" style={{animationDelay: '-12s'}}></div>
-          <div className="planet-2" style={{animationDelay: '-16s'}}></div>
+          {/* <div className="planet" style={{animationDelay: '-4s'}}></div> */}
+          {/* <div className="planet-1" style={{animationDelay: '-8s'}}></div> */}
+          {/* <div className="planet-1" style={{animationDelay: '-12s'}}></div> */}
+          {/* <div className="planet-2" style={{animationDelay: '-16s'}}></div> */}
         </div>
       </section>
     );
