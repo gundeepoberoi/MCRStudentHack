45d7d8ea86aba2eace319e64ca06773afe0caf2b
f57709jg <jacob.georgis@student.manchester.ac.uk>
2024-03-30 14:39:23 +0000

    Better the description of the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const roboto = Roboto({
 
 export const metadata: Metadata = {
   title: "StudentHack 2024",
-  description: "MCRStudentHack2024 organised by UniCS",
+  description: "Blast off with UniCS for Student Hack 2024, a student-led hackathon at the University of Manchester",
 };
 
 export default function RootLayout({
