98adf656c03e0c1227a3472f00db3cc2902626aa
Jacob Georgis <jacob.georgis@student.manchester.ac.uk>
2024-04-15 15:30:05 +0100

    Add Hackathons UK to sponsor section, rename section to sponsors and partners

--- src/components/sections/sponsors/sponsors.tsx
+++ src/components/sections/sponsors/sponsors.tsx
@@ -5,10 +5,11 @@ import './sponsors.css';
 export default function Sponsors() {
     return (
       <section id="sponsors" className="mt-48 mb-24 w-full z-10 flex flex-col items-center">
-        <h1 id="heading" className="text-5xl text-center">Sponsors</h1>
-        <div id="divider" className="flex flex-wrap w-full text-white rounded-lg overflow-hidden">
+        <h1 id="heading" className="text-5xl text-center">Sponsors and Partners</h1>
+        <div id="divider" className="flex flex-wrap w-full text-white rounded-lg">
           <div className="sun"></div>
           <div className="planet" style={{animationDelay: '0s', cursor: 'pointer'}} onClick={() => window.open('https://www.booking.com/', '_blank')}></div>
+          <div className="planet-1" style={{animationDelay: '0s', cursor: 'pointer'}} onClick={() => window.open('https://www.hackathons.org.uk/', '_blank')}></div>
           {/* <div className="planet" style={{animationDelay: '-4s'}}></div> */}
           {/* <div className="planet-1" style={{animationDelay: '-8s'}}></div> */}
           {/* <div className="planet-1" style={{animationDelay: '-12s'}}></div> */}
